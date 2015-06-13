f14740d7193c7b7358367b531556b70ce53fd973
Cynthia Lee <cynlee.9@gmail.com>
2015-06-12 23:51:13 -0700

    Set default lang to Arabic, and allow user to change language (#64)

--- src/angular/controllers/navbar.controller.js
+++ src/angular/controllers/navbar.controller.js
@@ -1,4 +1,12 @@
 var controllers = angular.module('controllers');
 
-controllers.controller('NavbarCtrl', ['$scope', function ($scope) {}]);
+controllers.controller('NavbarCtrl', ['$scope', '$translate', function ($scope, $translate) {
+  $scope.languages = ['AR','EN'];
+  $scope.selectedLanguage = 'AR';
+
+  $scope.changeLanguage = function (langKey) {
+    console.log('changeLanguage: ' + langKey);
+    $translate.use(langKey.toLowerCase());
+  };
+}]);
 
