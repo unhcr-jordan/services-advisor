59d0b41e3a819c7c864f94d13cf018c9fa4305dc
Jerry Chang <therealjerrycan@gmail.com>
2015-06-13 21:12:05 -0700

    tweaked the toggleSelection on the filter so map renders correctly [Jerry Chang]

--- src/angular/controllers/filter.controller.js
+++ src/angular/controllers/filter.controller.js
@@ -30,6 +30,8 @@ controllers.controller('FilterCtrl', ['$scope', 'Search', 'ServicesList', '_', f
                              .pluck("partnerName")
                              .unique()
                              .value();
+
+
     // Divide the organization names by half since we have two columns                         
     var splitValue = organizationsArray.length/2;    
     
@@ -43,38 +45,41 @@ controllers.controller('FilterCtrl', ['$scope', 'Search', 'ServicesList', '_', f
                                   })
                                  .toArray()
                                  .value();
-  
   }
  
   // calls the ServiceList function get which takes a call back function 
   // in this case we are collecting Organizations
   ServicesList.get(collectOrganizations);
 
+  // selected organizations
+  var selection = [];
   // toggle selection for a given organization by name
   $scope.toggleSelection = function toggleSelection(organization) {
-
-    // selected organizations
-    var selection = [];
-    
+   
     // stores the index of the organization currently being click
     var idx = selection.indexOf(organization);
-
     // is currently selected - splice that organization from selected array
     if (idx > -1) {
       selection.splice(idx, 1);
 
-      // Call search service to toggle that a certain partner was * un-selected *
-      Search.selectPartner(organization);
+      // A Hack Around to Re-render map when User Un-selectes items 
+      // TODO: *** Better solution: Have a Un-filter method in the Search Services for SelectPartner *** 
+      
+      // 1. Reset the Map
+      Search.clearAll();
+
+      // 2. Make Calls to select the remaining partners left in the selected Organization array defined 
+      _.each(selection, function(organizationSelected){
+        Search.selectPartner(organizationSelected);
+      })
       
     }
 
     // is newly selected - push organization into the selection array
     else {
       selection.push(organization);
-
       // Call search service to toggle that a certain partner was * selected * 
       Search.selectPartner(organization);
-
     }
     
   };
