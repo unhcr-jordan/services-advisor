3c2df6139e50f05f2c21c05f4bb9ce68dcbe87fb
Jerry Chang <therealjerrycan@gmail.com>
2015-06-14 00:58:06 -0700

    added a new applyFilter function to make the call to the 'Search' Service [Jerry Chang]

--- src/angular/controllers/filter.controller.js
+++ src/angular/controllers/filter.controller.js
@@ -11,22 +11,23 @@ controllers.controller('FilterCtrl', ['$scope', 'Search', 'ServicesList', '_', f
  // defines a function to callback function for filtering data 
   var collectOrganizations = function(data){
     
-    /*  
-        
-        This is a object containing Name of the partners as "KEY" and the relative 
-         Service Object ID associated with that partner as "VALUE" 
-          
-          Example:
+      /* 
+         Here we make use of methods in underscore 
 
-          Object {JWU: [342176915,1971267257], "IFH/NHF ": Array[11], ... , IMC: Array[6]}
+          chain - returns a wrapped object & calling methods on 
+                  it will continue to return wrapped objects 
+                  (calling VALUE will return final value)
+          pluck - extracting data from the objects inside the data array 
+          flatten 
+          unique - grabs the unique values 
     */
-    var partnerIdObject  = _.chain(data)
-                            .groupBy(function(obj){ return obj.properties.partnerName; })
-                            .mapObject( function(val, key) {return _.pluck(val, 'id') })
-                            .value();
+     organizationsArray   = _.chain(data) 
+                             .pluck("properties")
+                             // partnerName is same as 'Organization'
+                             .pluck("partnerName")
+                             .unique()
+                             .value();
   
-    // Gather the "KEY" values from PartnerIdObject which is the Partner/Organization Name 
-    var organizationsArray  = _.keys(partnerIdObject)
 
     // Divide the organization names by half since we have two columns                         
     var splitValue = organizationsArray.length/2;    
@@ -52,35 +53,30 @@ controllers.controller('FilterCtrl', ['$scope', 'Search', 'ServicesList', '_', f
   // toggle selection for a given organization by name
   $scope.toggleSelection = function toggleSelection(organization) {
    
-    // stores the index of the organization currently being click
-    var idx = selection.indexOf(organization);
+      // stores the index of the organization currently being click
+      var idx = selection.indexOf(organization);
     
-    // is currently selected - splice that organization from selected array
-    if (idx > -1) {
-      
-      selection.splice(idx, 1);
+      if (idx > -1) {
+      // is currently selected - splice that organization from selected array
+        selection.splice(idx, 1);
+      }
 
-      // A Hack Around to Re-render map when User Un-selectes items 
-      // TODO: *** Better solution: Have a Un-filter method in the Search Services for SelectPartner *** 
-      
-        // 1. Reset the Map
-        Search.clearAll();
-
-        // 2. Make Calls to select the remaining partners left in the selected Organization array defined 
-        _.each(selection, function(organizationSelected){
-          Search.selectPartner(organizationSelected);
-        })
-      
-    }
-
-    // is newly selected - push organization into the selection array
-    else {
-      selection.push(organization);
-      // Call search service to toggle that a certain partner was * selected * 
-      Search.selectPartner(organization);
-    }
+      // is newly selected - push organization into the selection array
+      else {
+        selection.push(organization);
+        // Call search service to toggle that a certain partner was * selected * 
+      }
     
   };
+
+  $scope.applyFilter = function(){
+      // 1. Reset the Map
+      Search.clearAll();
+      // 2. Make Calls to select the remaining partners left in the selected Organization array defined 
+      _.each(selection, function(organizationSelected){
+        Search.selectPartner(organizationSelected);
+      })
+  }
 
 
 }]);
